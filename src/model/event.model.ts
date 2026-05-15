8d24cf4981e3af8ad38cdf1c0a812a87ef42bc43
Fahim2023 <fahimshahrier2023@gmail.com>
2026-05-15 23:32:05 +0600

    code the new schema detailed things with tevent with export of model

--- src/model/event.model.ts
+++ src/model/event.model.ts
@@ -1,5 +1,18 @@
-import { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
+import { TEvent } from "../types/event.interface";
 
-const eventSchema = new Schema({
-  title: { type: String, required: true },
-});
+const eventSchema = new Schema<TEvent>(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    date: { type: Date, required: true },
+    location: { type: String, required: true },
+    organizer: { type: String, required: true },
+    image: String,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Event = model<TEvent>("Event", eventSchema);

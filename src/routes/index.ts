bb2f0cf2f17819a5a39e60af59980885dcb47843
Fahim2023 <fahimshahrier2023@gmail.com>
2026-05-16 23:54:20 +0600

    create the index-ts file inside the routes folder

---
+++ src/routes/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { AiRoutes } from "./ai.route";
+// import { EventRoutes } from "./event.route";
+import { UserRoutes } from "./user.route";
+import { EventRoutes } from "./event.routes";
+
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: "/events",
+    route: EventRoutes,
+  },
+  {
+    path: "/users",
+    route: UserRoutes,
+  },
+  {
+    path: "/ai",
+    route: AiRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
